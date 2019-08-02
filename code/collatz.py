0e6bab42209c0751d3cba2f723d618747fc2a9ca
BethanyG <BethanyG@users.noreply.github.com>
2019-08-01 20:11:56 -0700

    Update collatz.py

--- code/collatz.py
+++ code/collatz.py
@@ -1,16 +1,1 @@
-def collatz(number):
-    if number % 2 == 0:
-        number = number // 2
-        return number
 
-    elif number % 2 == 1:
-        number = 3 * number + 1
-        return number
-
-input_number = input("Type in a number.")
-input_number = int(input_number)
-answer = input_number
-
-while answer != 1:
-    answer = collatz(answer)
-    print(answer)
